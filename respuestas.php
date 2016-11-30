6bcf6057fcfafa1b46574fcf409aff118bc73f3a
davidcallizaya <davidcallizaya@192.168.1.5>
2016-11-30 13:12:25 -0400

    mejora del reporte de respuestas

--- respuestas.php
+++ respuestas.php
@@ -1,10 +1,48 @@
 <?php
 require_once './vendor/autoload.php';
 $krumo = new Krumo;
+$preguntas=[
+    'p1'=>'¿Utilizas o alguna vez utilizaste agenda impresa o digital?',
+    'p1porque'=>'¿Por qué?',
+    'p2'=>'¿Qué tipo de agenda es de su preferencia?',
+    'p2porque'=>'¿Por qué?',
+    'p3'=>'¿Qué le llama más la atención en una Agenda?',
+    'p3a'=>'¿Qué diseño le gusta más?',
+    'p3b'=>'¿Qué diseño le gusta más?',
+    'p3c'=>'¿Qué tamaño le gusta más?',
+    'p4'=>'¿Estaría interesada en conocer una agenda que le brinde consejos de salud, financieros y desarrollo personal?',
+    'p5'=>'Para usted una agenda debe ser:',
+    'p6'=>'Enumere la siguiente lista en orden de importancia (1 el más importante y 10 el menos importante):',
+    'p7'=>'¿Le gustaría que su agenda cuente con un espacio para guardar tarjetas personales, bancarias y fotografías?',
+    'p8'=>'¿Le gustaría recibir mayor información de la agenda y/o aplicación para celular?',
+];
+echo "<table border='1' style='font-size: 10pt'>";
+echo "<tr style='background-color: lightblue;'>";
+echo "<td>Nombre</td><td>Teléfono</td><td>Email</td><td>Edad</td>";
+foreach($preguntas as $p=>$lab){
+    echo "<th>$lab</th>";
+}
+echo "</tr>";
 foreach (glob('encuestas/*.txt') as $f) {
     $respuestas = json_decode(file_get_contents($f));
-    $name = basename($f,'.txt');
-    echo "<b><u>$name</u></b>";
-    Kint::dump($respuestas);
+    $nombre = @$respuestas->p9nombre->porque;
+    $telefono = @$respuestas->p9telefono->porque;
+    $email = @$respuestas->p9email->porque;
+    $edad = @$respuestas->datos->edad;
+    echo "<tr>";
+    echo "<td>$nombre</td><td>$telefono</td><td>$email</td><td>$edad</td>";
+    foreach($preguntas as $p=>$lab){
+        $res = '';
+        $res .= isset($respuestas->$p->respuesta)? $respuestas->$p->respuesta :'';
+        $res .= isset($respuestas->$p->porque)? $respuestas->$p->porque :'';
+        if($p=='p3a' || $p=='p3b') {
+            $res=$res.'<img style="width:2em" class="slide" src="images/'.$res.'.jpg">';
+        } elseif($p=='p3c') {
+            $a=['p3g'=>'A4','p3h'=>'A5','p3i'=>'A6'];
+            $res=@$a[$res].'<img style="width:2em" class="slide" src="images/'.$res.'.jpg">';
+        }
+        echo "<td>$res</td>";
+    }
+    echo "</tr>";
 }
-
+echo "</table>";

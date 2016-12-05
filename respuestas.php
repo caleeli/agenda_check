25487ca46da19f6c4f26ea6a8986e96c2a6efb7e
davidcallizaya <davidcallizaya@192.168.1.5>
2016-12-05 10:06:19 -0400

    divide p6

--- respuestas.php
+++ respuestas.php
@@ -26,9 +26,22 @@ $preguntas = [
 ];
 echo "<table border='1' style='font-size: 10pt'>";
 echo "<tr style='background-color: lightblue;'>";
-echo "<th>#</th><th>Nombre</th><th>Teléfono</th><th>Email</th><th>Edad</th>";
+echo "<th>Num</th><th>cantidad</th><th>Nombre</th><th>Teléfono</th><th>Email</th><th>Edad</th>";
 foreach ($preguntas as $p => $lab) {
-    echo "<th>$lab</th>";
+    if($p=='p6') {
+        echo "<th>TAMANIO</th>";
+        echo "<th>PESO</th>";
+        echo "<th>DISENIO DE LA TAPA</th>";
+        echo "<th>PLANIFICADOR</th>";
+        echo "<th>GUIA DE RESTAURANTES SALONES</th>";
+        echo "<th>NOTAS</th>";
+        echo "<th>FRASES Y NOTAS INSPIRADORAS</th>";
+        echo "<th>GUIA TELEFONICA</th>";
+        echo "<th>CONSEJOS DE SALUD y BELLEZA</th>";
+        echo "<th>STIKERS</th>";
+    } else {
+        echo "<th>$lab</th>";
+    }
 }
 echo "</tr>";
 $row = 0;
@@ -40,18 +53,42 @@ foreach (glob('encuestas/*.txt') as $f) {
     $email = @$respuestas->p9email->porque;
     $edad = @$respuestas->datos->edad;
     echo "<tr>";
-    echo "<th>$row</th><td>$nombre</td><td>$telefono</td><td>$email</td><td>$edad</td>";
+    echo "<th>$row</th><td>1</td><td>$nombre</td><td>$telefono</td><td>$email</td><td>$edad</td>";
     foreach ($preguntas as $p => $lab) {
         $res = '';
         $res .= isset($respuestas->$p->respuesta) ? $respuestas->$p->respuesta : '';
         $res .= isset($respuestas->$p->porque) ? $respuestas->$p->porque : '';
         if ($p == 'p3a' || $p == 'p3b') {
             $res = empty($res) ? '' : ($res);
+            echo "<td>$res</td>";
         } elseif ($p == 'p3c') {
             $a = ['p3g' => 'A4', 'p3h' => 'A5', 'p3i' => 'A6'];
             $res = empty($a[$res]) ? '' : ($a[$res]);
+            echo "<td>$res</td>";
+        } elseif ($p == 'p6') {
+            if(preg_match_all('/\((\d+)\) ([^\n\(]+)/', $res, $matches)) {
+                foreach ($matches[1] as $d) {
+                    if($d=='0') {
+                        echo "<td></td>";
+                    } else {
+                        echo "<td>$d</td>";
+                    }
+                }
+            } else {
+                echo "<td></td>";
+                echo "<td></td>";
+                echo "<td></td>";
+                echo "<td></td>";
+                echo "<td></td>";
+                echo "<td></td>";
+                echo "<td></td>";
+                echo "<td></td>";
+                echo "<td></td>";
+                echo "<td></td>";
+            }
+        } else {
+            echo "<td>$res</td>";
         }
-        echo "<td>$res</td>";
     }
     echo "</tr>";
 }

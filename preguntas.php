ea705c89b132cf39b28259e1b64a895648c8146d
davidcallizaya <davidcallizaya@192.168.1.5>
2017-10-30 13:56:41 -0400

    encuentas

---
+++ preguntas.php
@@ -0,0 +1,22 @@
+<?php
+header('Content-Type:application/json');
+?>[
+    {
+        "n": 1,
+        "q": "Que tan solo?",
+        "o": [
+            {"l":"mucho"},
+            {"l":"poco"},
+            {"l":"nada"}
+        ]
+    },
+    {
+        "n": 2,
+        "q": "Que onda?",
+        "o": [
+            {"l":"A"},
+            {"l":"B"},
+            {"l":"C","o":true}
+        ]
+    }
+]
